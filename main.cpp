d6ca96ffdd069b4fe02685e9967d6f2efa79a31a
sergei <foxu726@gmail.com>
2024-12-16 23:54:41 +0500

    нашёл смещение до начала первого заголовка

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <vector>
 
 #pragma pack(push, 1)
 
@@ -23,13 +24,33 @@ uint8_t UnpVer;
 uint8_t Method;
 uint16_t NameSize;
 uint32_t FileAttr;
-
 };
 
-void read_headers() {
+void read_headers(vector<char> data) {
+
+    VolumeHeader* p_header = reinterpret_cast<VolumeHeader*>(&data[7]);
+    cout << hex << int(p_header->header_size) << endl;
+    int offset = 7+int(p_header->header_size);
+    cout << hex << offset << endl;
+    while (true){
+    VolumeHeader* p_header = reinterpret_cast<VolumeHeader*>(&data[offset]); //перебор заголовков
+    File_Head* f_header = reinterpret_cast<File_Head*>(&data[offset+7]);
+    cout << hex << int(f_header->NameSize) << endl;
+    uint16_t fileName = int(f_header->NameSize);
+
+    cout << hex << offset << endl;
+    uint16_t fileNameOffset = sizeof(struct File_Head) + offset + sizeof(struct VolumeHeader);
+    cout << hex << fileNameOffset << endl;
+
+
+
+    //cout << hex << int(f_header->PackSize) << endl;
+    //offset = int(p_header->header_size)+int(f_header->PackSize);
+    //cout << hex << offset << endl;
+    break;
+    }
 
 };
-
 
 
 #pragma pack(pop)
@@ -43,6 +64,10 @@ int main(){
         int fileSize = rar_file.tellg();
         cout << "File size: " << fileSize << endl;
         rar_file.seekg(0, ios::beg);
+        vector<char> rar_data(fileSize, 0);
+        rar_file.read(rar_data.data(), fileSize);
+        read_headers(rar_data);
+
 
     }
 return 0;
