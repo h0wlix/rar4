0eb899f89a9ba2084afc315c2c612d25cb50f8c4
sergei <foxu726@gmail.com>
2024-12-16 23:56:11 +0500

    добавил пояснения и сделал вывод всех заголовков

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstdint>
 #include <vector>
+#include <windows.h>
 
 #pragma pack(push, 1)
 
@@ -28,45 +29,46 @@ uint32_t FileAttr;
 
 void read_headers(vector<char> data) {
 
-    VolumeHeader* p_header = reinterpret_cast<VolumeHeader*>(&data[7]);
-    cout << hex << int(p_header->header_size) << endl;
-    int offset = 7+int(p_header->header_size);
-    cout << hex << offset << endl;
+    VolumeHeader* p_header = reinterpret_cast<VolumeHeader*>(&data[7]); //заполнили структуру начиная с седьмого байта
+
+    int offset = 7+int(p_header->header_size); //пропускаем сигнатуру и перемещаемся на первый заголовок
+
     while (true){
-    VolumeHeader* p_header = reinterpret_cast<VolumeHeader*>(&data[offset]); //перебор заголовков
-    File_Head* f_header = reinterpret_cast<File_Head*>(&data[offset+7]);
-    cout << hex << int(f_header->NameSize) << endl;
-    uint16_t fileName = int(f_header->NameSize);
 
-    cout << hex << offset << endl;
-    uint16_t fileNameOffset = sizeof(struct File_Head) + offset + sizeof(struct VolumeHeader);
-    cout << hex << fileNameOffset << endl;
+        VolumeHeader* p_header = reinterpret_cast<VolumeHeader*>(&data[offset]); //перебор заголовков
+        File_Head* f_header = reinterpret_cast<File_Head*>(&data[offset+7]); //начинаем считывать данные в структуру со второго заголовка
 
 
+        uint16_t fileName = int(f_header->NameSize); //узнаём размер названия заголовка
+        uint16_t fileNameOffset = sizeof(struct File_Head) + offset + sizeof(struct VolumeHeader); //отступ файл нейма
+        const char* firstHeaderName = reinterpret_cast<const char*>(&data[fileNameOffset]); //создаёт указатель на смещение файл нейма
+        string utf8line(firstHeaderName, fileName);
+        vector<wchar_t> utf16content(fileName);
+        int wideCharCount = MultiByteToWideChar(CP_UTF8, 0, utf8line.c_str(), fileName, utf16content.data(), utf16content.size());
+        wstring hideName(utf16content.begin(), utf16content.end());
+        wcout << "Hide content:" << hideName << endl;
 
-    //cout << hex << int(f_header->PackSize) << endl;
-    //offset = int(p_header->header_size)+int(f_header->PackSize);
-    //cout << hex << offset << endl;
-    break;
+        offset += int(p_header->header_size)+int(f_header->PackSize);
+
     }
 
-};
 
+};
 
 #pragma pack(pop)
 
 int main(){
-    ifstream rar_file("D:\\dz\\rar4\\Example.rar", ios::binary);
+    ifstream rar_file("D:\\dz\\rar4\\Example.rar", ios::binary); //открыли файл
 
-    if (rar_file.is_open()){
+    if (rar_file.is_open()){ //проверка открытия файла
         cout << "open ok" << endl;
-        rar_file.seekg(0, ios::end);
-        int fileSize = rar_file.tellg();
+        rar_file.seekg(0, ios::end); //переместились в конец
+        int fileSize = rar_file.tellg(); //узнали текущее положение указателя
         cout << "File size: " << fileSize << endl;
-        rar_file.seekg(0, ios::beg);
-        vector<char> rar_data(fileSize, 0);
-        rar_file.read(rar_data.data(), fileSize);
-        read_headers(rar_data);
+        rar_file.seekg(0, ios::beg); //переместились в начало
+        vector<char> rar_data(fileSize, 0); //создали буфер размера файла и заполнили нулями
+        rar_file.read(rar_data.data(), fileSize); //заполняем буфер содержимым файла
+        read_headers(rar_data); //вызываем функцию
 
 
     }
